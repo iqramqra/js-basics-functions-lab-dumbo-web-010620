f6d604065518d003c74f4498745dbddf28e79e77
Iqra Masroor <masrooriqra@gmail.com>
2020-02-16 05:32:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+  //returns the number of blocks given a value
+}
+ 
+function distanceFromHqInFeet (someValue) {
+  distanceFromHqInBlocks(someValue);
